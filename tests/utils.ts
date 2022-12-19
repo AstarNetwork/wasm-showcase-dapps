6d0107e684bb13caa11792d0b8c0b846082dc1eb
Hyunggyu Jang <murasakipurplez5@gmail.com>
2022-12-19 22:41:09 +0900

    Fix dex spec

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,13 @@
+import type { WeightV2 } from '@polkadot/types/interfaces'
+import { ApiPromise } from '@polkadot/api';
 export function parseUnits(amount: bigint | number, decimals = 18): bigint {
   return BigInt(amount) * 10n ** BigInt(decimals);
+}
+
+export function scaleWeightV2(api: ApiPromise, weight: WeightV2, scale: number): WeightV2 {
+  return api.registry.createType(
+    'WeightV2', {
+    refTime: (weight as WeightV2).refTime.unwrap().muln(scale),
+    proofSize: (weight as WeightV2).proofSize.unwrap().muln(scale),
+  }) as WeightV2
 }
